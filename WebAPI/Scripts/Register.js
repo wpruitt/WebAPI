7f432d04ce29618832a3c7fa44d2687971e9b06d
Willie D. Pruitt Jr <w_pruitt@hotmail.com>
2018-01-21 19:04:30 -0600

    merged fixed and rebased master into current working branch -- stashed changes

---
+++ WebAPI/Scripts/Register.js
@@ -0,0 +1,43 @@
+﻿$("#registerBtn").click(function () {
+    var registerData = {
+        Email: $("#registerEmail").val(),
+        Password: $("#registerPassword").val(),
+        ConfirmPassword: $("#confirmPassword").val(),
+        //FirstName: $("#registerFirstName").val(),
+        //LastName: $("#registerLastName").val(),
+        //Address1: $("#registerAddress1").val(),
+        //Address2: $("#registerAddress2").val(),
+        //City: $("#registerCity").val(),
+        //State: $("#registerState").val(),
+        //ZipCode: $("#registerZipCode").val(),
+        //DOB: $("#registerDOB").val(),
+        //PhoneNumber: $("#registerPhoneNumber").val()
+    };
+    var regLogData = {
+        grant_type: 'password',
+        username: $("#registerEmail").val(),
+        password: $("#registerPassword").val()
+    };
+    console.log("registerData", registerData);
+    console.log("data-stringify", JSON.stringify(registerData));
+    $.ajax({
+        type: 'POST',
+        url: '/api/Account/Register',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(registerData)
+    }).done(function (data) {
+        console.log("Registered!");
+        $.ajax({
+            type: 'POST',
+            url: '/Token',
+            data: regLogData
+        }).done(function (data) {
+            sessionStorage.setItem('tokenKey', data.access_token);
+            console.log('and logged in')
+        }).fail(function (error) {
+            console.log('error', error);
+        });
+    }).fail(function (error) {
+        console.log('error', error);
+    })
+});
